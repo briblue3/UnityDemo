8d87434671b3cc7bc3d7f377c6f02df19ad522b6
briblue3 <brianna.hill.3@live.com>
2017-01-04 14:05:27 -0500

    InitialCommit
    Created new Unity project.

---
+++ New Unity Project/Assets/Scripts/CubeController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+	public int speed;
+	public Rigidbody rg;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
